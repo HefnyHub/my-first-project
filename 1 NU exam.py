260d163ff8ab9a456121f2996098baffbf9294cb
Ahmed Hefny <a.hefny1997@gmail.com>
2025-08-09 07:48:18 +0300

    1 NU exam

---
+++ 1 NU exam.py
@@ -0,0 +1,19 @@
+# my first code in python
+for i in range(3):
+    print("Hello Ahmed")
+# say Hi for My friends:
+friends = ['salama', 'farag', 'zyad']
+for friend in friends:
+    print("Hello " + friend)
+name = "Ahmed"
+print("Hi " + name)
+name = "Farag"
+print("Hi "+ name )
+X = 20 
+Y = 30
+print(X+Y)
+##############
+A = " Ahmed"
+H = " Hefny"
+print( "Dev" + A + H )
+
--- first project.py
+++ first project.py
@@ -1,5 +1,46 @@
-print(" hello worled ")
+for i in range(3):
+ print("Hello, friends!")
 # my first code in python
-git add .
-git commit -m "first one "
-git push
+friends = ['farag', 'zyad', 'abdelrahman', 'salama']
+for friend in friends:
+    print("Hi " + friend)
+    name = "Ahmed"
+print ("hello "+name)
+#Hello Ahmed
+print(4+5)
+#9
+print(9*7)
+#63
+print(-1/4)
+#-0.25
+print(1/3)
+#0.3333333333333333
+print(((2050/5)-32)/9)
+#42.0
+print(2**10)
+#1024
+############################
+# Syntax for printing a string of text
+print("I love Python!")
+# Syntax for printing numeric values
+print(360)
+print(32*45)
+# Syntax for printing the value of a variable
+value = 8*6
+print(value)
+############################
+# Multiplication, division, addition, and subtraction
+print(3*8/2+5-1)
+ 
+# Exponents
+print(4**6) # Syntax means 4 to the power of 6
+print(4**2) # To square a number
+print(4**3) # To cube a number
+print(4**0.5) # To find the square root of a number
+# To calculate how many different possible combinations can be
+# formed using a set of "x" characters with each character in "x"
+# having "y" number of possible values, you will need to use an 
+# exponent for the calculation:
+x = 4
+y = 26
+print(y*x)
